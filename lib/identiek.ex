62caeba5cdf65cafdb3b994b58187dcc0bd358e2
Sushant Sharma <sushant.wurk@gmail.com>
2024-07-01 18:08:57 +0530

    pixel_map

--- lib/identiek.ex
+++ lib/identiek.ex
@@ -11,8 +11,9 @@ defmodule Identiek do
     input
     |> calculate_hash()
     |> get_color()
-    |> chunk_list(3, 5)
     |> build_grid()
+    |> fliter_out_odd()
+    |> build_pixel_map()
   end
 
   defp calculate_hash(input) do
@@ -23,24 +24,49 @@ defmodule Identiek do
     %Identiek.Image{hex: hex}
   end
 
-  defp get_color(%Identiek.Image{color: _, hex: h}) do
+  defp get_color(%Identiek.Image{hex: h} = image) do
     color = Enum.take(h, 3)
-    %Identiek.Image{hex: h, color: color}
+    %Identiek.Image{image | color: color}
   end
 
-  defp chunk_list(%Identiek.Image{color: _, hex: h}, chunk, take) do
-    h
-    |> Enum.chunk_every(chunk)
-    |> Enum.take(take)
-  end
-
-  defp build_grid(list) do
+  defp build_grid(%Identiek.Image{hex: hex_list} = image) do
     # INFO: &mirror_row(&1) / &mirror_row/1 same as fn l ->  mirror_row(l) end
-    Enum.map(list, &mirror_row/1)
+    grid =
+      hex_list
+      |> Enum.chunk_every(3)
+      |> Enum.take(5)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identiek.Image{image | grid: grid}
   end
 
   defp mirror_row(list) do
     [first, second | _tail] = list
     list ++ [second, first]
+  end
+
+  defp fliter_out_odd(%Identiek.Image{grid: grid} = image) do
+    g =
+      grid
+      |> Enum.filter(fn {val, _} -> Integer.mod(val, 2) == 0 end)
+
+    %Identiek.Image{image | grid: g}
+  end
+
+  defp build_pixel_map(%Identiek.Image{grid: grid} = image) do
+    pixel_map =
+      Enum.map(grid, fn {_val, idx} ->
+        horizontal = rem(idx, 5) * 50
+        vertical = div(idx, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identiek.Image{image | pixel_map: pixel_map}
   end
 end
