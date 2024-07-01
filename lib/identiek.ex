fa7894ba138a3423dfcb15739abab9e690aad019
Sushant Sharma <sushant.wurk@gmail.com>
2024-07-01 16:43:05 +0530

    color, chunk & build grid

--- lib/identiek.ex
+++ lib/identiek.ex
@@ -11,6 +11,8 @@ defmodule Identiek do
     input
     |> calculate_hash()
     |> get_color()
+    |> chunk_list(3, 5)
+    |> build_grid()
   end
 
   defp calculate_hash(input) do
@@ -21,8 +23,24 @@ defmodule Identiek do
     %Identiek.Image{hex: hex}
   end
 
-  defp get_color(struct) do
-    hex = struct
-    Enum.take(hex.hex, 3)
+  defp get_color(%Identiek.Image{color: _, hex: h}) do
+    color = Enum.take(h, 3)
+    %Identiek.Image{hex: h, color: color}
+  end
+
+  defp chunk_list(%Identiek.Image{color: _, hex: h}, chunk, take) do
+    h
+    |> Enum.chunk_every(chunk)
+    |> Enum.take(take)
+  end
+
+  defp build_grid(list) do
+    # INFO: &mirror_row(&1) same as fn l ->  mirror_row(l) end
+    Enum.map(list, &mirror_row(&1))
+  end
+
+  defp mirror_row(list) do
+    [first, second | _tail] = list
+    list ++ [second, first]
   end
 end
